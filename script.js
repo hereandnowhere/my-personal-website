56d860fdb0bdbb519e9df871e507b08f16b5845c
Jacob Vine <jvine1235@gmail.com>
2022-01-22 11:54:43 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+
+h2.textContent = "This is a place holder paragraph. I am still brainstorming what I wish to place here. We shall see! Very exciting times here in here and no where land!";
+h2.style.color = "f0ffff";
+h2.style.textAlign = "center";
+
+document.querySelector("body").appendChild(h2);
