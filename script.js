33d2f503527fdd516dca2816760208a306cf3eb5
Jacob Vine <jvine1235@gmail.com>
2022-01-22 14:36:12 -0500

    First draft, 1-22-2022

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "This is a place holder paragraph. I am still brainstorming what I wish to place here. We shall see! Very exciting times here in here and no where land!";
+h2.textContent = "Performing Artist // Artistic Programmer";
 h2.style.color = "f0ffff";
 h2.style.textAlign = "center";
 
